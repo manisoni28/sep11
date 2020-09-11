9569fd050974106688e1bb05684b9cd622645526
Soni <mani_soni@intuit.com>
2020-09-11 18:29:50 +0530

    added dummy activitiy

---
+++ app/src/main/java/com/android/plagochat/ui/DummyGitActivity.java
@@ -0,0 +1,16 @@
+package com.android.plagochat.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.plagochat.R;
+
+public class DummyGitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy_git);
+    }
+}
