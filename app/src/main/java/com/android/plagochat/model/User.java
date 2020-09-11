540f9da71cad405194e23327259f19102b8a9d3e
Soni <mani_soni@intuit.com>
2020-09-11 18:25:59 +0530

    added complete plagochat

---
+++ app/src/main/java/com/android/plagochat/model/User.java
@@ -0,0 +1,31 @@
+package com.android.plagochat.model;
+
+
+
+public class User {
+    public String name;
+    public String email;
+    public String avata;
+    public Status status;
+    public Message message;
+
+
+    public User(){
+        status = new Status();
+        message = new Message();
+        status.isOnline = false;
+        status.timestamp = 0;
+        message.idReceiver = "0";
+        message.idSender = "0";
+        message.text = "";
+        message.timestamp = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
